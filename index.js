2cb3e77d022f84df4417fa553011730b6cdc8ecc
AbrilBorrasFerre <abril.borras@institutroquetes.cat>
2025-05-30 22:56:29 +0200

    tanca sessio

--- index.js
+++ index.js
@@ -95,6 +95,8 @@ function nou_usuari() {
 function tanca_sessio() {
     if (validat) {
         if (confirm("Vols tancar la sessió?")) {    // S'ha respost "Sí"
+            validat = false;
+            location.reload();
             storage.setItem("usuari", "");
             location.reload();    // recàrrega de la pàgina, es reinicialitzen totes les variables
         }
